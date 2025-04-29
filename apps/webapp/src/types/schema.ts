e1940f5457af15e2a38567caccd09ec31c2ed9f0
thisisnich <74246822+thisisnich@users.noreply.github.com>
2025-04-29 13:10:29 +0800

    Update schema.ts

--- apps/webapp/src/types/schema.ts
+++ apps/webapp/src/types/schema.ts
@@ -1,7 +1,7 @@
 export type AllocationType = 'amount' | 'percentage' | 'overflow';
 
 export interface Allocation {
-  _id?: string; // Optional because it may not exist when creating a new allocation
+  _id: string; // Optional because it may not exist when creating a new allocation
   category: string;
   type: AllocationType;
   value: number;
